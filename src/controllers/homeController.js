104d87bd4b69b46e6ea90aa229f27833df3af6be
nngiaminh1812 <21120501@student.hcmus.edu.vn>
2023-12-27 13:47:25 +0700

    DB setup, test connect DB

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,16 @@
-let getHomePage = (req, res) => {
-  return res.render("home.ejs");
+import db from '../models/index'
+
+let getHomePage = async (req, res) => {
+
+  try {
+    let data = await db.User.findAll()
+    console.log(data)
+    console.log('--------')
+    return res.render("home.ejs", {data: JSON.stringify(data)});
+  } catch (error) {
+    console.log(error)
+  }
+  
 };
 
 let getAboutPage = (req, res) => {
